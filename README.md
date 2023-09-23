a79a9d4af1a9b2e4e4da166e0159501744d06891
KagemaNjoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2023-09-23 16:38:19 +0300

    Initialize project
    Update project Description

---
+++ README.md
@@ -0,0 +1,27 @@
+# iContacts - a minialist phone book
+
+## Description
+
+This is a simple phone book application that allows users to add, edit, and delete contacts. The application is built Flutter and uses Firebase for authentication and storage.
+
+## Installation
+
+- Clone the repository
+
+```bash
+git clone https://kagemanjoroge/contacts.git
+```
+
+- Install dependencies
+
+```bash
+flutter pub get
+```
+
+- Run the application
+
+```bash
+flutter run
+```
+
+If you like this project, please leave a star 🌟
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const Home(),
+      theme: ThemeData(useMaterial3: true),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('iContacts'),
+      ),
+    );
+  }
+}
